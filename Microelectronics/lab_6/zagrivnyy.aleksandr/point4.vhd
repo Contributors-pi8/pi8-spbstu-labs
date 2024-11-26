d254f1a063d5dcaa6f02a9ff827cb005a983f65e
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2024-11-26 23:21:18 +0300

    fix: Change wrong files in ME lab 6

--- Microelectronics/lab_6/zagrivnyy.aleksandr/point4.vhd
+++ Microelectronics/lab_6/zagrivnyy.aleksandr/point4.vhd
@@ -4,12 +4,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity point4 is
     Port (
         en  : in  STD_LOGIC;
-        a   : in  STD_LOGIC_VECTOR (3 downto 0);
-        y   : out STD_LOGIC_VECTOR (15 downto 0)
+        a   : in  STD_LOGIC_VECTOR (5 downto 0);
+        y   : out STD_LOGIC_VECTOR (63 downto 0)
     );
 end point4;
 
-architecture behav of point4 is
+architecture Behavioral of point4 is
     component point3
         Port (
             en  : in  STD_LOGIC;
@@ -18,22 +18,25 @@ architecture behav of point4 is
         );
     end component;
 
-    signal y_lower : STD_LOGIC_VECTOR(7 downto 0);
-    signal y_upper : STD_LOGIC_VECTOR(7 downto 0);
+    signal y_segments : STD_LOGIC_VECTOR(63 downto 0);
+    signal enable_signals : STD_LOGIC_VECTOR(7 downto 0);
+
 begin
-    lower_decoder: point3
+    decoder_enable: point3
         Port map (
-            en => en AND NOT a(3),
-            a  => a(2 downto 0),
-            y  => y_lower
+            en => en,
+            a => a(5 downto 3),
+            y => enable_signals
         );
 
-    upper_decoder: point3
-        Port map (
-            en => en AND a(3),
-            a  => a(2 downto 0),
-            y  => y_upper
-        );
+    gen_decoders: for i in 0 to 7 generate
+        decoder_inst: point3
+            Port map (
+                en => not enable_signals(i),
+                a  => a(2 downto 0),
+                y  => y_segments(i*8+7 downto i*8)
+            );
+    end generate;
 
-    y <= y_lower & y_upper;
-end behav;
+    y <= y_segments;
+end Behavioral;
